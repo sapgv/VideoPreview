defd3add4d6606fb41e407bff2034011a7678b67
Grigory Sapogov <grisha.sapgv@mail.ru>
2025-06-30 20:33:14 +0500

    Добавил перетаскивание

--- VideoPreview/ViewController.swift
+++ VideoPreview/ViewController.swift
@@ -9,11 +9,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let videoPreview = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.title = "Call"
+        self.setupVideoPreview()
+        self.setupView()
+        self.setupConstraints()
+        self.setupPanGesture()
     }
-
 
 }
 
+private extension ViewController {
+    func setupVideoPreview() {
+        self.videoPreview.backgroundColor = .systemGreen
+        self.videoPreview.layer.cornerRadius = 8
+        self.videoPreview.clipsToBounds = true
+    }
+    
+    func setupView() {
+        self.view.addSubview(videoPreview)
+    }
+    
+    func setupConstraints() {
+        videoPreview.translatesAutoresizingMaskIntoConstraints = false
+        videoPreview.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        videoPreview.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        videoPreview.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        videoPreview.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+    }
+    
+    func setupPanGesture() {
+        let panGesture = UIPanGestureRecognizer()
+        panGesture.addTarget(self, action: #selector(panGestureCall))
+        videoPreview.addGestureRecognizer(panGesture)
+    }
+    
+    @objc
+    func panGestureCall(_ sender: UIPanGestureRecognizer) {
+        let point = sender.translation(in: view) // mark 3
+        videoPreview.center = CGPoint(x: videoPreview.center.x + point.x, y: videoPreview.center.y + point.y)
+        sender.setTranslation(CGPoint.zero, in: view)
+    }
+}
